c38b2ac3695ade35680f372e21d30c09b1f88d34
Sameer-coder <sameeransari93057@gmail.com>
2025-06-29 15:22:28 +0530

    change to comment out

--- volumeOfaSphere.c
+++ volumeOfaSphere.c
@@ -1,23 +1,23 @@
-// #include<stdio.h>
-// int main(){
-//     int r = 99;
-//     float v = 4*3.14*r*r*r/3;
-//     printf("The value of volume: %f " ,v);
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int r = 99;
+    float v = 4*3.14*r*r*r/3;
+    printf("The value of volume: %f " ,v);
+    return 0;
+}
 
-// #include<stdio.h>
-//  int main()
-// {
-//     int r = 6;
+#include<stdio.h>
+ int main()
+{
+    int r = 6;
 
-//     float A = 4*3.14*r*r;
+    float A = 4*3.14*r*r;
 
-//     printf("Area of circle: %f",A);
+    printf("Area of circle: %f",A);
 
 
 
-//     return 0;
-// }
+    return 0;
+}
 
 
