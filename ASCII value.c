12c851c9aa52d97e1b96cef28b95baf4e24e16dd
Sameer-coder <sameeransari93057@gmail.com>
2025-06-30 10:04:41 +0530

    add something in char data type file and add ASCII also

---
+++ ASCII value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    char ch = 'a';
+
+    printf(" %d ", ch);  
+}                         //it gives the ascii value of a is 97 by these way we can find ascii value of any letter may alphabate also.
+
+
+
--- Char data type.c
+++ Char data type.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-   char ch = 'a';
+   char ch = 'a';   //here in place of 'a' can 'any alphabate  also may capital letter'
 
    printf("%c",ch);
 
